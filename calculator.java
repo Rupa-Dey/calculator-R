4360f3204484f26a9e50721a4c49285209f16d92
Rupa Dey <deyrupa525@gmail.com>
2021-11-22 15:59:20 +0600

    subtraction added

--- calculator.java
+++ calculator.java
@@ -4,18 +4,24 @@ public class calculator{
 	{
 		int result = sum(6,8);
 		int div = division(10,30);
+		int sub = subtraction(12,5);
 
 		System.out.println(result);
 		System.out.println(div);
+		system.out.println(sub);
 	}
 	static int sum(int x,int y)
 	{
 		return x+y;
 	}
-
 	static int division(int x,int y)
 	{
 		return y/x;
 	}
+	static int subtraction(int x,int y)
+	{
+		return x-y;
+	}
+
 	
 }
