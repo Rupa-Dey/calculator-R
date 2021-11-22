033031718ff2b002f6ab5c2c4fb2e1d36e61115c
Rupa Dey <deyrupa525@gmail.com>
2021-11-22 15:38:56 +0600

    first  commit

---
+++ calculator.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+public class calculator{
+	public static void main(String[] args)
+	{
+		int result = sum(6,8);
+
+		System.out.println(result);
+	}
+	static int sum(int x,int y)
+	{
+		return x+y;
+	}
+	
+}
