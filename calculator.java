4be60ff483f417d4c0191da36accdc2fbc8d5b2f
Rupa Dey <deyrupa525@gmail.com>
2021-11-22 15:48:12 +0600

    division part added

--- calculator.java
+++ calculator.java
@@ -3,12 +3,19 @@ public class calculator{
 	public static void main(String[] args)
 	{
 		int result = sum(6,8);
+		int div = division(10,30);
 
 		System.out.println(result);
+		System.out.println(div);
 	}
 	static int sum(int x,int y)
 	{
 		return x+y;
+	}
+
+	static int division(int x,int y)
+	{
+		return y/x;
 	}
 	
 }
